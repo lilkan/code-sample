633c1e392bc9a29fd589fdd86bc4ba68fd36837a
Герман Бежев <doooomen@gmail.com>
2022-04-01 20:25:29 +0300

    Updates

---
+++ _nuxt/bb135dd.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{263:function(n,e,t){"use strict";t.r(e);var l={name:"IndexPage"},w=t(49),component=Object(w.a)(l,(function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[t("nuxt-baba"),n._v("\n  qwdqwdqw\n")],1)}),[],!1,null,null,null);e.default=component.exports}}]);
