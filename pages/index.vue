895f0fc1fd3b0c5c461c0c0508e95053f80134d4
Герман Бежев <doooomen@gmail.com>
2022-04-01 16:42:10 +0300

    init

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+import NuxtBaba from '~/components/base/NuxtBaba.vue'
+export default {
+  components: { NuxtBaba },
+  name: 'IndexPage'
+}
+</script>
